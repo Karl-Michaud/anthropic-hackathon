56d5fc8f18f27f83bc6d828e985a9b270b06a79a
Leo Duan <leoduan0@icloud.com>
2025-11-18 21:18:26 -0500

    fix storybook imports; add Navigation to storybook

--- app/stories/ScholarshipInformationBlock.stories.ts
+++ app/stories/ScholarshipInformationBlock.stories.ts
@@ -1,4 +1,4 @@
-import type { Meta, StoryObj } from '@storybook/react'
+import type { Meta, StoryObj } from '@storybook/nextjs'
 import ScholarshipInformationBlock from './ScholarshipInformationBlock'
 
 const meta: Meta<typeof ScholarshipInformationBlock> = {
